ce29e417efd996060303f2ebccbdff22bfd1f484
vaishnave28 <152139901+vaishnave28@users.noreply.github.com>
2023-12-29 21:59:04 +0530

    Add files via upload

---
+++ linear search.cpp
@@ -0,0 +1,28 @@
+
+#include <stdio.h>
+
+int linearSearch(int arr[], int size, int target) {
+    for (int i = 0; i < size; i++) {
+        if (arr[i] == target) {
+            return i;  // Return the index where the target is found
+        }
+    }
+
+    return -1;  // Return -1 if the target is not found
+}
+
+int main() {
+    int arr[] = {5, 10, 3, 8, 7};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int target = 8;
+
+    int result = linearSearch(arr, size, target);
+
+    if (result != -1) {
+        printf("Element %d found at index %d\n", target, result);
+    } else {
+        printf("Element %d not found in the array\n", target);
+    }
+
+    return 0;
+}
